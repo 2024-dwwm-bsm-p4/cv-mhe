e1c63e92d5f9d6a71bdb96b973304964ff2b4f12
Maxence. <hermax625@gmail.com>
2024-09-17 16:15:26 +0200

    Update star.js

--- js/star.js
+++ js/star.js
@@ -1,22 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
     const starContainer = document.getElementById('stars-container');
     
-    // Fonction pour générer des étoiles
+  
     function generateStars(count) {
         for (let i = 0; i < count; i++) {
             const star = document.createElement('div');
             star.classList.add('star');
             
-            // Positionnement aléatoire des étoiles
-            star.style.left = Math.random() * 100 + 'vw'; // Position horizontale aléatoire
-            star.style.top = Math.random() * 100 + 'vh'; // Position verticale initiale aléatoire
-            star.style.animationDuration = Math.random() * 5 + 5 + 's'; // Durée d'animation aléatoire entre 5s et 10s
+
+            star.style.left = Math.random() * 100 + 'vw'; 
+            star.style.top = Math.random() * 100 + 'vh';
+            star.style.animationDuration = Math.random() * 5 + 5 + 's'; 
             
-            // Ajouter l'étoile au conteneur
+            
             starContainer.appendChild(star);
         }
     }
 
-    // Générer 100 étoiles
+    
     generateStars(100);
 });
