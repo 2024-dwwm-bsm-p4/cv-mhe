a0fcb733162a5220329006e0b5a966e150b7bcf9
Maxence. <hermax625@gmail.com>
2024-09-18 09:23:23 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
+    if (localStorage.getItem('darkMode') === 'enabled') {
+        document.body.classList.add('dark');
+    }
+
     document.getElementById('darkModeToggle').addEventListener('click', function() {
         document.body.classList.toggle('dark');
-      
+        
+        if (document.body.classList.contains('dark')) {
+            localStorage.setItem('darkMode', 'enabled');
+        } else {
+            localStorage.removeItem('darkMode');
+        }
     });
 });
